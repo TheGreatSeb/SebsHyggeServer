0b191c0ba83655ef97a8a17303c091450a0a8025
BLumbye <benjamin@lumbye.net>
2019-06-24 16:20:25 +0200

    Public page

---
+++ precache-manifest.1a63ceea18286d1ef9eb1190cc6fb576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8210c1eb7b97fe16cfff",
+    "url": "/js/chunk-vendors.933c92b6.js"
+  },
+  {
+    "revision": "81d661bf05164f04d806",
+    "url": "/js/app.a8724a81.js"
+  },
+  {
+    "revision": "2689615a4243eefeddbb07c86b3c7953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54186fdf22258c8a177940fdd1534864",
+    "url": "/img/worldmap.54186fdf.svg"
+  },
+  {
+    "revision": "8210c1eb7b97fe16cfff",
+    "url": "/css/chunk-vendors.67cb45cc.css"
+  },
+  {
+    "revision": "81d661bf05164f04d806",
+    "url": "/css/app.35064cc1.css"
+  }
+];
--- src/Bus.js
+++
@@ -1,4 +0,0 @@
-// This is an event bus - it is imported by files and used to receive and emit events
-import Vue from 'vue';
-
-export default new Vue();
